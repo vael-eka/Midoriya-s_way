051ee0af038e6229304ed60b130289e46007f7d1
Alvarado <moisoar@amazon.com>
2026-07-22 14:13:08 -0600

    first exercise

--- daily_exercises/day1.py
+++ daily_exercises/day1.py
@@ -1,3 +1,13 @@
 """
-La concha
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
 """
+
+
+class Solution(object):
+    def two_sum(self, nums, target):
+        # Hash map to store {number_value: index}
+        seen = {}
